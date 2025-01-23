3fd88a1cf8e351a999a9a0fd0acb973262b8f197
syeed111 <c193067@ugrad.iiuc.ac.bd>
2025-01-23 18:55:48 +0600

    configuration for deployment

--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -3,12 +3,20 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { app, server } from "./socket/socket.js";
+import path from "path";
 dotenv.config();
-const app = express();
+//const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes); //don't forget the front slash/api
 app.use("/api/messages", messageRoutes);
-app.listen(5001, () => {
-    console.log("listening on port 5000");
+if (process.env.NODE_ENV !== "development") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+server.listen(5001, () => {
+    console.log("listening on port 5001");
 });
